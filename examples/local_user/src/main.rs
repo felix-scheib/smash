4fbecb44f40463b70d5e83935fe04b1af3087538
Felix Scheib <felixscheib@outlook.de>
2024-09-01 16:45:58 +0200

    ADD: read measurements added

--- examples/local_user/src/main.rs
+++ examples/local_user/src/main.rs
@@ -1,12 +1,7 @@
 #[cfg(target_os = "hermit")]
 use hermit as _;
 
-use std::{
-    env,
-    net::UdpSocket,
-    thread,
-    time::{Duration, Instant},
-};
+use std::{env, hint::black_box, net::UdpSocket, time::Instant};
 
 use smash::{config::Config, shared_memory::SharedMemory};
 use tracing::info;
@@ -21,14 +16,15 @@ const N: usize = 1_000;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let _config = Config::new(&args);
 
     let send = UdpSocket::bind(SEND_IP).unwrap_or_log();
     info!("Bind sender to: {:?}", send);
     let recv = UdpSocket::bind(RECV_IP).unwrap_or_log();
     info!("Bind receiver to: {:?}", recv);
 
-    let sm = SharedMemory::new(&config.get_hosts(), send, recv);
+    let hosts = vec!["10.0.5.255:4202".to_owned()];
+    let sm = SharedMemory::new(&hosts, send, recv);
     info!("Shared Memory initialized! Writer started");
 
     let mut times = Vec::with_capacity(N);
@@ -55,32 +51,17 @@ fn main() {
 
     info!("{:#?}", times);
 
-    /*
-    loop {
+    let start = Instant::now();
+
+    for _ in 0..N {
         let start = Instant::now();
-        let first = sm.memory_layout.first();
 
-        {
-            let mut value = first.write();
-            value[0] = 42;
-        }
-
-        first.update();
-
-        let duration = start.elapsed();
-        info!("Write operation took: {}us", duration.as_micros());
-
-        let start = Instant::now();
-        let first = sm.memory_layout.first();
-
-        {
+        black_box({
             let _value = first.read();
-        }
+        });
 
-        let duration = start.elapsed();
-        info!("Read operation took: {}us", duration.as_micros());
-
-        thread::sleep(Duration::from_secs(1));
+        times.push(start.elapsed());
     }
-    */
+
+    info!("{:#?}", start.elapsed());
 }
